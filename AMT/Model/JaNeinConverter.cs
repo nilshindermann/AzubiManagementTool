534904c7b0595ef53b591545a20478ce9a5ce2de
Nils Hindermann <nilshindermann@gmail.com>
2022-01-19 16:29:16 +0100

    Mittwochnachmittag
    Alle Properties von Lerneneden lesbar und änderbar machen

---
+++ AMT/Model/JaNeinConverter.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace AMT.Model
+{
+    public class JaNeinConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is bool yesNo)
+            {
+                if (yesNo == true)
+                {
+                    return "Ja";
+                }
+                else
+                {
+                    return "Nein";
+                }
+            }
+            return "Nein";
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            switch (value?.ToString()?.ToLower())
+            {
+                case "ja":
+                case "yes":
+                case "oui":
+                    return true;
+                case "nein":
+                case "no":
+                case "non":
+                    return false;
+            }
+            return false;
+        }
+    }
+}
