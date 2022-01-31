4c66f723e61394b7ef30d4a6440e336323bce1fe
Nils Hindermann <nilshindermann@gmail.com>
2022-01-31 11:54:41 +0100

    Validierung Eingabefelder Teil 1 #1

--- AMTCore/Models/Lernende.cs
+++ AMTCore/Models/Lernende.cs
@@ -1,4 +1,5 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
 namespace AMTCore.Models
@@ -6,8 +7,41 @@ namespace AMTCore.Models
     /// <summary>
     /// Model-Klasse für einen Lernenden mit vielen Informationen
     /// </summary>
-    public class Lernende
+    public class Lernende : IDataErrorInfo
     {
+        public string Error => string.Empty;
+        public string this[string name]
+        {
+            get
+            {
+                if (name == nameof(Vorname) && string.IsNullOrWhiteSpace(Vorname))
+                {
+                    return "Der Vorname fehlt";
+                }
+                else if (name == nameof(Nachname) && string.IsNullOrWhiteSpace(Nachname))
+                {
+                    return "Der Nachname fehlt";
+                }
+                else if (name == nameof(Geschlecht) && string.IsNullOrWhiteSpace(Geschlecht))
+                {
+                    return "Das Geschlecht fehlt";
+                }
+                else if (name == nameof(Email) && string.IsNullOrWhiteSpace(Email))
+                {
+                    return "Die E-Mail-Adresse fehlt";
+                }
+                else if (name == nameof(Beruf) && string.IsNullOrWhiteSpace(Beruf))
+                {
+                    return "Die Fachrichtung fehlt";
+                }
+                else if (name == nameof(Lehrfirma) && Lehrfirma == null)
+                {
+                    return "Die Lehrfirma fehlt";
+                }
+                return string.Empty;
+            }
+        }
+
         [Key]
         public int Id { get; set; }
 
