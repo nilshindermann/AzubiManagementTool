935bb5ef8d9252041edaa6b05bacc779cd4052ec
Nils Hindermann <nilshindermann@gmail.com>
2022-01-31 16:25:16 +0100

    Validierung Note
    #1

--- AMTCore/Models/Note.cs
+++ AMTCore/Models/Note.cs
@@ -1,12 +1,38 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 
 namespace AMTCore.Models
 {
     /// <summary>
     /// Model-Klasse für eine Note eines Lernenden in einem Modul
     /// </summary>
-    public class Note
+    public class Note : IDataErrorInfo
     {
+        public string Error => string.Empty;
+        public string this[string name]
+        {
+            get
+            {
+                if (name == nameof(Lernender) && Lernender == null)
+                {
+                    return "Lernender fehlt";
+                }
+                else if (name == nameof(Wert) && (Wert < 1.0 || Wert > 6.0))
+                {
+                    return "Illegaler Notenwert";
+                }
+                else if (name == nameof(Gewicht) && Gewicht < 0)
+                {
+                    return "Gewicht kann nicht negativ sein";
+                }
+                else if (name == nameof(Modul) && (Modul < 100 || Modul > 999))
+                {
+                    return "Falsches Format für Modul";
+                }
+                return string.Empty;
+            }
+        }
+
         [Key]
         public int Id { get; set; }
         public Lernende? Lernender { get; set; }
