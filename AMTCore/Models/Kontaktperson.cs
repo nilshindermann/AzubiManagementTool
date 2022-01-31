b86c11c4d79d39251547fadf367991cb7e67fcb9
Nils Hindermann <nilshindermann@gmail.com>
2022-01-31 16:41:41 +0100

    Validierung Kontaktperson
    #1

--- AMTCore/Models/Kontaktperson.cs
+++ AMTCore/Models/Kontaktperson.cs
@@ -1,4 +1,5 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
 namespace AMTCore.Models
@@ -6,8 +7,33 @@ namespace AMTCore.Models
     /// <summary>
     /// Model-Klasse für eine Kontaktperson einer Lehrfirma
     /// </summary>
-    public class Kontaktperson
+    public class Kontaktperson : IDataErrorInfo
     {
+        public string Error => string.Empty;
+        public string this[string name]
+        {
+            get
+            {
+                if (name == nameof(Vorname) && string.IsNullOrWhiteSpace(Vorname))
+                {
+                    return "Der Vorname fehlt";
+                }
+                else if (name == nameof(Nachname) && string.IsNullOrWhiteSpace(Nachname))
+                {
+                    return "Der Nachname fehlt";
+                }
+                else if (name == nameof(Email) && string.IsNullOrWhiteSpace(Email))
+                {
+                    return "Die E-Mail-Adresse fehlt";
+                }
+                else if (name == nameof(Lehrfirma) && Lehrfirma == null)
+                {
+                    return "Die Lehrfirma fehlt";
+                }
+                return string.Empty;
+            }
+        }
+
         [Key]
         public int Id { get; set; }
 
