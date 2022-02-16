099b9f41ca877b5ebc4eb37cf911ffec9a98d600
Nils Hindermann <nilshindermann@gmail.com>
2022-02-16 12:36:26 +0100

    Anzeige Modulprüfung

---
+++ AMT/Model/IfTrueStringConverter.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace AMT.Model
+{
+    /// <summary>
+    /// Klasse zum Konvertieren von Boolean nach eigenem String wenn TRUE
+    /// </summary>
+    public class IfTrueStringConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is bool yesNo)
+            {
+                if (yesNo == true)
+                {
+                    return parameter?.ToString() ?? string.Empty;
+                }
+            }
+            return string.Empty;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return !string.IsNullOrWhiteSpace(value?.ToString());
+        }
+    }
+}
